8d2a194e63cad451c4ec7afd218e97c65fdb949c
Anton Tivonenko <anton.tivonenko@gmail.com>
2025-02-12 19:57:10 +0300

    Файл конфигурации версии расширения

---
+++ config/.version.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Этот файл является частью расширения модуля веб-приложения GearMagic.
+ * 
+ * Файл конфигурации версии расширения.
+ * 
+ * @link https://gearmagic.ru
+ * @copyright Copyright (c) 2015 Веб-студия GearMagic
+ * @license https://gearmagic.ru/license/
+ */
+
+return [
+    'name'        => 'Interface element properties',
+    'description' => 'Properties of Control panel interface elements',
+    'version'     => '1.0',
+    'versionDate' => '20-12-2017',
+    'author'      => 'GearMagic',
+    'authorUrl'   => 'https://gearmagic.ru',
+    'email'       => 'anton.tivonenko@gmail.com',
+    'url'         => 'https://apps.gearmagic.ru/component/gm-be-references-properties',
+    'license'     => 'The MIT License',
+    'licenseUrl'  => 'https://apps.gearmagic.ru/license/mit-license.html'
+];
