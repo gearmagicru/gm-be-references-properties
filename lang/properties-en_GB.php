7f5ea8463e0445b757374473b555fde1d740b2a3
Anton Tivonenko <anton.tivonenko@gmail.com>
2025-02-12 19:56:22 +0300

    Файлы локализации

---
+++ lang/properties-en_GB.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Этот файл является частью расширения модуля веб-приложения GearMagic.
+ * 
+ * Пакет русской локализации.
+ * 
+ * @link https://gearmagic.ru
+ * @copyright Copyright (c) 2015 Веб-студия GearMagic
+ * @license https://gearmagic.ru/license/
+ */
+
+return [
+    'Width' => 'Ширина',
+    'Height' => 'Высота'
+];
---
+++ lang/text-en_GB.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Этот файл является частью расширения модуля веб-приложения GearMagic.
+ * 
+ * Пакет английской (британской) локализации.
+ * 
+ * @link https://gearmagic.ru
+ * @copyright Copyright (c) 2015 Веб-студия GearMagic
+ * @license https://gearmagic.ru/license/
+ */
+
+return [
+    '{name}'        => 'Interface element properties',
+    '{description}' => 'Properties of Control panel interface elements',
+    '{permissions}' => [
+        'any'  => ['Full access', 'View and make changes to element properties'],
+        'view' => ['View', 'Viewing element properties'],
+        'read' => ['Reading', 'Reading element properties']
+    ],
+
+    // Grid: контекстное меню записи
+    'Edit record' => 'Edit record',
+    // Grid: столбцы
+    'Name' => 'Name',
+    'Property' => 'Property',
+    'Type' => 'Type',
+    // типы значений
+    'boolean' => 'Boolean',
+    'date' => 'Date',
+    'number' => 'Number',
+    'string' => 'String',
+
+    // Form
+    '{form.title}' => 'Adding an element property',
+    '{form.titleTpl}' => 'Edit an element property "{name}"'
+];
